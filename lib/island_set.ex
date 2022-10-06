ef801a492e0a9fb020ed013750b48718ca2b5b60
hanzy1110 <eloser1994@gmail.com>
2022-10-06 08:48:07 -0300

    board2

--- lib/island_set.ex
+++ lib/island_set.ex
@@ -4,10 +4,10 @@ defmodule IslandsEngine.IslandSet do
   defstruct atoll: :none, dot: :none, l_shape: :none, s_shape: :none, square: :none
 
   def start_link() do
-    Agent.start_link(fn -> %IslandSet{} end)
+    Agent.start_link(fn -> initialize_set() end)
   end
 
-  def initialize_set() do
+  defp initialize_set() do
     Enum.reduce(keys(), %IslandSet{}, fn key, acc ->
       {:ok, island} = Island.start_link()
       Map.put(acc, key, island)
@@ -18,5 +18,15 @@ defmodule IslandsEngine.IslandSet do
     %IslandSet{}
     |> Map.from_struct()
     |> Map.keys()
+  end
+
+  def to_string(island_set) do
+    "%IslandSet{" <> string_body(island_set) <> "}"
+  end
+
+  defp string_body(island_set) do
+    Enum.reduce(keys(), "", fn key, acc ->
+      nil
+    end)
   end
 end
