f71793eca612442db4f1d2ad2e9ba1ffed661d3f
hanzy1110 <eloser1994@gmail.com>
2022-10-07 22:09:21 -0300

    board6

--- lib/rules.ex
+++ lib/rules.ex
@@ -2,7 +2,19 @@ defmodule IslandsEngine.Rules do
   @behaviour :gen_statem
   alias IslandsEngine.Rules
 
-  def init(:ok) do
+  def show_current_state(fsm) do
+    :gen_statem.call(fsm, :show_current_state)
+  end
+
+  def add_player(fsm) do
+    :gen_statem.call(fsm, :add_player)
+  end
+
+  def start_link do
+    :gen_statem.start_link(__MODULE__, :initialized, [])
+  end
+
+  def init(_) do
     {:ok, :initialized, []}
   end
 
@@ -14,13 +26,17 @@ defmodule IslandsEngine.Rules do
 
   def terminate(_reason, _state, _data), do: :nothing
 
+  def initialized({:call, from}, :show_current_state, state_data) do
+    {:next_state, :initialized, state_data, {:reply, from, :initialized}}
+  end
+
   def initialized({:call, from}, :add_player, state_data) do
     {:next_state, :players_set, state_data, {:reply, from, :ok}}
   end
 
-  def initialized({:call, from}, :show_current_state, state_data) do
-    {:keep_state_and_data, :players_set, state_data, {:reply, from, :ok}}
-  end
+  # def initialized({:call, from}, :show_current_state, state_data) do
+  #   {:keep_state_and_data, :players_set, state_data, {:reply, from, :ok}}
+  # end
 
   def initialized({:call, from}, _, _state_data) do
     {:keep_state_and_data, {:reply, from, :error}}
@@ -36,7 +52,64 @@ defmodule IslandsEngine.Rules do
     end
   end
 
-  def start_link do
-    :gen_statem.start_link(__MODULE__, :initialized, [])
+  def players_set({:call, from}, {:set_islands, player}, state_data) do
+    state_data = Map.put(state_data, player, :islands_set)
+    set_islands_reply(from, state_data, state_data.player1, state_data.player2)
+  end
+
+  def players_set({:call, from}, :show_current_state, _state_data) do
+    {:keep_state_and_data, {:reply, from, :ok}}
+  end
+
+  def players_set({:call, from}, _, _state_data) do
+    {:keep_state_and_data, {:reply, from, :error}}
+  end
+
+  def player1_turn({:call, from}, {:guess_coordinate, :player1}, state_data) do
+    {:next_state, :player2_turn, state_data, {:reply, from, :ok}}
+  end
+
+  def player1_turn({:call, from}, :win, state_data) do
+    {:next_state, :game_over, state_data, {:reply, from, :ok}}
+  end
+
+  def player1_turn({:call, from}, :show_current_state, _state_data) do
+    {:keep_state_and_data, {:reply, from, :player1_turn}}
+  end
+
+  def player1_turn({:call, from}, _, _state_data) do
+    {:keep_state_and_data, {:reply, from, :error}}
+  end
+
+  def player2_turn({:call, from}, {:guess_coordinate, :player2}, state_data) do
+    {:next_state, :player2_turn, state_data, {:reply, from, :ok}}
+  end
+
+  def player2_turn({:call, from}, :win, state_data) do
+    {:next_state, :game_over, state_data, {:reply, from, :ok}}
+  end
+
+  def player2_turn({:call, from}, :show_current_state, _state_data) do
+    {:keep_state_and_data, {:reply, from, :player2_turn}}
+  end
+
+  def player2_turn(_event, _caller_pid, state) do
+    {:reply, {:error, :action_out_of_sequence}, :player2_turn, state}
+  end
+
+  def game_over({:call, from}, :show_current_state, _state_data) do
+    {:keep_state_and_data, {:reply, from, :game_over}}
+  end
+
+  def game_over({:call, from}, _, _state_data) do
+    {:keep_state_and_data, {:reply, from, :error}}
+  end
+
+  defp set_islands_reply(from, state_data, status, status) when status == :islands_set do
+    {:next_state, :player1_turn, state_data, {:reply, from, :ok}}
+  end
+
+  defp set_islands_reply(from, state_data, _, _) do
+    {:keep_state, state_data, {:reply, from, :ok}}
   end
 end
